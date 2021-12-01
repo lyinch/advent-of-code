5c955daaa2a61d328a4a46184d83fc21fb5c86c0
Backes Thierry <backes.thierry@gmail.com>
2021-12-01 11:32:14 +0100

    Move individual days to yearly directory

---
+++ 2021/day01-b/target/debug/day01-b.d
@@ -0,0 +1,1 @@
+/Users/backes/Documents/Dev/aoc/day01-b/target/debug/day01-b: /Users/backes/Documents/Dev/aoc/day01-b/src/main.rs
---
+++ 2021/day01-b/target/debug/deps/day01_b-fafc37aea87eb8ae.d
@@ -0,0 +1,5 @@
+/Users/backes/Documents/Dev/aoc/day01-b/target/debug/deps/day01_b-fafc37aea87eb8ae: src/main.rs
+
+/Users/backes/Documents/Dev/aoc/day01-b/target/debug/deps/day01_b-fafc37aea87eb8ae.d: src/main.rs
+
+src/main.rs:
