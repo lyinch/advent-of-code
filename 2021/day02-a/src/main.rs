9d73307061776a46a363e50814c100abdab35544
Backes Thierry <backes.thierry@gmail.com>
2021-12-02 11:33:05 +0100

    Add day 2

---
+++ 2021/day02-a/src/main.rs
@@ -0,0 +1,32 @@
+use std::io::{BufReader, BufRead};
+use std::fs::File;
+
+struct SubPosition{
+    x: i32,
+    y: i32,
+}
+
+
+
+fn main() {
+    let br = BufReader::new(File::open("input.txt").unwrap());
+    let input = br.lines();
+    let mut position = SubPosition{x:0, y:0};
+    for line in input {
+        let l = line.unwrap();
+        let data: Vec<&str> = l.split_whitespace().collect();
+        match data[..] {
+            ["forward", n] => {
+                position.x += n.parse::<i32>().unwrap();
+            },
+            ["up", n] => {
+                position.y -= n.parse::<i32>().unwrap();
+            },
+            ["down", n] => {
+                position.y += n.parse::<i32>().unwrap();
+            },
+            _ => {}
+        }
+    }
+   println!("{}", position.x*position.y);
+}
